7f66e83306152a2f5bcbd282d4aed0721361789a
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2022-07-31 10:20:00 -0400

    Added Wakatime and Github Copilot to Astrovim

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -44,53 +44,18 @@ local config = {
   -- Set colorscheme
   colorscheme = "gruvbox",
 
-  -- Override highlight groups in any theme
---  highlights = {
-    -- duskfox = { -- a table of overrides
-    --   Normal = { bg = "#000000" },
-    -- },
-  --  default_theme = function(highlights) -- or a function that returns one
-    --  local C = require "default_theme.colors"
-      --highlights.Normal = { fg = C.fg, bg = C.bg }
---      return highlights
-    -- end,
-  --},
-
   -- set vim options here (vim.<first_key>.<second_key> =  value)
   options = {
     opt = {
+      guifont = "MesloLGS NF:h13",
       relativenumber = true, -- sets vim.opt.relativenumber
     },
     g = {
+      minimap_width = 10,
+      minimap_auto_start = 1,
+      minimap_auto_start_win_enter = 1,
       mapleader = " ", -- sets vim.g.mapleader
     },
-  },
-
-default_theme = {
-    colors = function(C)
-      C.telescope_green = C.green
-      C.telescope_red = C.red
-      C.telescope_fg = C.fg
-      C.telescope_bg = C.black_1
-      C.telescope_bg_alt = C.bg_1
-      return C
-    end,
-    highlights = function(hl)
-      local C = require "default_theme.colors"
-      hl.TelescopeBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg }
-      hl.TelescopeNormal = { bg = C.telescope_bg }
-      hl.TelescopePreviewBorder = { fg = C.telescope_bg, bg = C.telescope_bg }
-      hl.TelescopePreviewNormal = { bg = C.telescope_bg }
-      hl.TelescopePreviewTitle = { fg = C.telescope_bg, bg = C.telescope_green }
-      hl.TelescopePromptBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg_alt }
-      hl.TelescopePromptNormal = { fg = C.telescope_fg, bg = C.telescope_bg_alt }
-      hl.TelescopePromptPrefix = { fg = C.telescope_red, bg = C.telescope_bg_alt }
-      hl.TelescopePromptTitle = { fg = C.telescope_bg, bg = C.telescope_red }
-      hl.TelescopeResultsBorder = { fg = C.telescope_bg, bg = C.telescope_bg }
-      hl.TelescopeResultsNormal = { bg = C.telescope_bg }
-      hl.TelescopeResultsTitle = { fg = C.telescope_bg, bg = C.telescope_bg }
-      return hl
-    end,
   },
 
   -- Disable AstroNvim ui features
@@ -106,6 +71,11 @@ default_theme = {
      -- "catppuccin/nvim",
       "ellisonleao/gruvbox.nvim",
       "folke/lsp-colors.nvim",
+      "wfxr/minimap.vim",
+      -- Wakatime should ask you for your api key, if not, run WakaTimeApiKey
+      "wakatime/vim-wakatime",
+      -- For github copilot, you must run :Copilot setup / and have node 17 installed
+      "github/copilot.vim",
       -- You can disable default plugins as follows:
       -- ["goolord/alpha-nvim"] = { disable = true },
 
@@ -254,7 +224,6 @@ default_theme = {
   -- This function is run last
   -- good place to configuring augroups/autocommands and custom filetypes
   polish = function()
-    vim.cmd([[set guifont=MesloLGS\ NF:h13]])
     -- Set key binding
     -- Set autocommands
     vim.api.nvim_create_augroup("packer_conf", { clear = true })
