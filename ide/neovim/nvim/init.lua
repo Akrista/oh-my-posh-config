438f2a4cb83d6e901e1abad373083e8d2823fd74
Jorge Thomas (Akrista) <23145794+Akrista@users.noreply.github.com>
2023-07-13 19:41:27 -0400

    Update on Neovim

--- ide/neovim/nvim/init.lua
+++ ide/neovim/nvim/init.lua
@@ -1,6 +1,6 @@
 --[[
 =====================================================================
-========================== NOTAS DE AYUDA ===========================
+=============================== NOTES ===============================
 =====================================================================
 
 - Leader Key: es la tecla que se usa para ejecutar comandos en Neovim, por defecto es la tecla <space>
@@ -40,18 +40,18 @@ vim.g.maplocalleader = ' '
 --    Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
 vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: Here is where you install your plugins.
@@ -59,8 +59,9 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
-require('lazy').setup(
+require("lazy").setup(
   {
+
     -- NOTE: First, some plugins that don't require any configuration
 
     -- Git related plugins
@@ -70,11 +71,32 @@ require('lazy').setup(
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
 
-    -- Better comments
-    -- 'devanlooches/better-comments-nvim',
+    -- Copilot
+    { 'github/copilot.vim' },
 
-    -- NOTE: This is where your plugins related to LSP can be installed.
-    --  The configuration is done below. Search for lspconfig to find it below.
+    -- Wakatime
+    { 'wakatime/vim-wakatime' },
+    {
+      'morhetz/gruvbox'
+    },
+    {
+      -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    },
+    {
+      -- Set lualine as statusline
+      'nvim-lualine/lualine.nvim',
+      -- See `:help lualine.txt`
+      opts = {
+        options = {
+          icons_enabled = false,
+          theme = 'gruvbox',
+          component_separators = '|',
+          section_separators = '',
+        },
+      },
+    },
     {
       -- LSP Configuration & Plugins
       -- Language Server, estos ofrecen ayuda en la escritura de diferentes lenguajes de programación.
@@ -85,35 +107,40 @@ require('lazy').setup(
         -- Para abrirlo solo debes ejecutar :Mason en la consola
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
-
+        {
+          'jay-babu/mason-null-ls.nvim',
+          event = { 'BufReadPre', 'BufNewFile' },
+          dependencies = {
+            'jose-elias-alvarez/null-ls.nvim',
+          },
+          config = function()
+            require("mason-null-ls").setup({
+              ensure_installed = {
+                -- Formatters, Linters and Debuggers
+                "autopep8",
+                "sqlfluff",
+                "sql-formatter",
+                "hadolint",
+                "prettierd",
+                "eslint_d",
+                "js-debug-adapter",
+                "cpptools"
+              }
+            })
+          end
+        },
         -- Useful status updates for LSP
         -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
         -- Fidget se encarga de mostrar las actualizaciones y calculos que realiza un LSP
         -- podras observar el spinner de carga en la parte inferior derecha
-        { 'j-hui/fidget.nvim', opts = {} },
+        { 'j-hui/fidget.nvim', opts = {}, tag = "legacy" },
 
         -- Additional lua configuration, makes nvim stuff amazing!
         'folke/neodev.nvim',
       },
     },
-
-    {
-      -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-    },
-
-    -- Jukit for Jupyter Notebooks
-    -- { 'luk400/vim-jukit' },
-
-    -- Copilot
-    { 'github/copilot.vim' },
-
-    -- Wakatime
-    { 'wakatime/vim-wakatime' },
-
     -- Useful plugin to show you pending keybinds.
-    { 'folke/which-key.nvim', opts = {} },
+    { 'folke/which-key.nvim',  opts = {} },
     {
       -- Adds git releated signs to the gutter, as well as utilities for managing changes
       'lewis6991/gitsigns.nvim',
@@ -128,29 +155,6 @@ require('lazy').setup(
         },
       },
     },
-
-    {
-      'morhetz/gruvbox',
-      priority = 1000,
-      config = function()
-        vim.cmd.colorscheme 'gruvbox'
-      end,
-    },
-
-    {
-      -- Set lualine as statusline
-      'nvim-lualine/lualine.nvim',
-      -- See `:help lualine.txt`
-      opts = {
-        options = {
-          icons_enabled = false,
-          theme = 'gruvbox',
-          component_separators = '|',
-          section_separators = '',
-        },
-      },
-    },
-
     {
       -- Add indentation guides even on blank lines
       'lukas-reineke/indent-blankline.nvim',
@@ -161,10 +165,18 @@ require('lazy').setup(
         show_trailing_blankline_indent = false,
       },
     },
-
     -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim',         opts = {} },
-
+    { 'numToStr/Comment.nvim', opts = {} },
+    {
+      -- Highlight, edit, and navigate code
+      'nvim-treesitter/nvim-treesitter',
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+      config = function()
+        pcall(require('nvim-treesitter.install').update { with_sync = true })
+      end,
+    },
     -- Fuzzy Finder (files, lsp, etc)
     -- Telescope es una herramienta que ayuda en la busqueda de archivos, palabras, segmentos de codigo, etc...
     { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
@@ -181,42 +193,83 @@ require('lazy').setup(
         return vim.fn.executable 'make' == 1
       end,
     },
-
     {
-      -- Highlight, edit, and navigate code
-      --
-      'nvim-treesitter/nvim-treesitter',
-      dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-      },
+      'neovim/nvim-lspconfig',
       config = function()
-        pcall(require('nvim-treesitter.install').update { with_sync = true })
+        -- Switch for controlling whether you want autoformatting.
+        --  Use :KickstartFormatToggle to toggle autoformatting on or off
+        local format_is_enabled = true
+        vim.api.nvim_create_user_command('KickstartFormatToggle', function()
+          format_is_enabled = not format_is_enabled
+          print('Setting autoformatting to: ' .. tostring(format_is_enabled))
+        end, {})
+
+        -- Create an augroup that is used for managing our formatting autocmds.
+        --      We need one augroup per client to make sure that multiple clients
+        --      can attach to the same buffer without interfering with each other.
+        local _augroups = {}
+        local get_augroup = function(client)
+          if not _augroups[client.id] then
+            local group_name = 'kickstart-lsp-format-' .. client.name
+            local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+            _augroups[client.id] = id
+          end
+
+          return _augroups[client.id]
+        end
+
+        -- Whenever an LSP attaches to a buffer, we will run this function.
+        --
+        -- See `:help LspAttach` for more information about this autocmd event.
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
+          -- This is where we attach the autoformatting for reasonable clients
+          callback = function(args)
+            local client_id = args.data.client_id
+            local client = vim.lsp.get_client_by_id(client_id)
+            local bufnr = args.buf
+
+            -- Only attach to clients that support document formatting
+            if not client.server_capabilities.documentFormattingProvider then
+              return
+            end
+
+            -- Tsserver usually works poorly. Sorry you work with bad languages
+            -- You can remove this line if you know what you're doing :)
+            if client.name == 'tsserver' then
+              return
+            end
+
+            -- Create an autocmd that will run *before* we save the buffer.
+            --  Run the formatting command for the LSP that has just attached.
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = get_augroup(client),
+              buffer = bufnr,
+              callback = function()
+                if not format_is_enabled then
+                  return
+                end
+
+                vim.lsp.buf.format {
+                  async = false,
+                  filter = function(c)
+                    return c.id == client.id
+                  end,
+                }
+              end,
+            })
+          end,
+        })
       end,
     },
-
-    -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-    --       These are some example plugins that I've included in the kickstart repository.
-    --       Uncomment any of the lines below to enable them.
-    require 'kickstart.plugins.autoformat',
-    require 'kickstart.plugins.debug',
-    -- require 'kickstart.keymaps',
-
-    -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-    --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-    --    up-to-date with whatever is in the kickstart repo.
-    --
-    --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-    --
-    --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
-    --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-    -- { import = 'custom.plugins' },
-  }, {})
+  }
+  , {})
 
 -- ! [[ Setting options ]]
 -- See `:help vim.o`
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 
 -- Make line numbers default
 vim.wo.number = true
@@ -276,6 +329,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -311,13 +365,14 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go',
+    'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
   highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
+  indent = { enable = true, disable = { --[['python']] } },
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -430,15 +485,19 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- tsserver = {},
-  intelephense = {},
+  pyright = {},
+  tsserver = {},
   lemminx = {},
-  zls = {},
   sqlls = {},
   rust_analyzer = {},
+  bashls = {},
+  cssls = {},
+  dockerls = {},
+  docker_compose_language_service = {},
+  html = {},
+  jsonls = {},
+  marksman = {},
+  powershell_es = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -451,6 +510,7 @@ local servers = {
 require('neodev').setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
@@ -473,6 +533,11 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+
+--vim.cmd("let g:gruvbox_transparent_bg = 1")
+--vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+vim.cmd.colorscheme 'gruvbox'
+
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
@@ -518,6 +583,3 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
